2d20a09ff18b979d8d58eaf0e3494d5f76bf8325
Imon <contact@imonweb.co.uk>
2023-12-11 11:17:14 +0000

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  /*  Get the value theme from the browser */
+  const current_theme = localStorage.getItem('current_theme');
+  // const [theme,setTheme] = useState('light');
+  const [theme,setTheme] = useState(current_theme ? current_theme : 'light');
+
+  /*  toggle icon - update the theme from local storage */
+  useEffect(() => {
+    localStorage.setItem('current_theme', theme);
+  },[theme])
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
   )
 }
 
